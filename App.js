cb3d0d2713b77ce763e934b3c05ce8c937fb1f09
unknown <nguyenducminh21042000@gmail.com>
2022-04-05 09:40:39 +0700

    update

--- App.js
+++ App.js
@@ -4,26 +4,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { store } from './src/redux/store';
-import allReducer from "./src/reducers";
-import { View, Text, Button, TouchableOpacity, TextInput } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useSelector, useDispatch } from "react-redux";
-import { configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
-
-import productReducer from './src/reducers/productReducer'
-import userReducer from './src/reducers/userReducer'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import MainTap from './src/screen/mainTap'
 import Login from './src/screen/login'
 
 const Stack = createNativeStackNavigator();
 
-
 function App() {
-
 
   let persistor = persistStore(store)
   return (
